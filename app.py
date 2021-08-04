495eb12aa2a11b4e2d11c756d612b1248dca236a
clementlelievre <clement.lelievre90@gmail.com>
2021-08-04 12:10:16 +0200

    Kmeans part of the app

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
-import numpy as np
 import streamlit as st
-import matplotlib.pylab as plt
+import matplotlib.pyplot as plt
+from sklearn.cluster import KMeans
 from utils import *
 
 st.set_page_config(
@@ -9,17 +9,37 @@ st.set_page_config(
     layout="centered", # wide
     initial_sidebar_state="auto") # collapsed
 
-st.title('DecisionTreeRegressor vs Adaboost')
+st.set_option('deprecation.showPyplotGlobalUse', False) #prevent an annoying banner related to an error when plotting
 
-n_est = st.sidebar.slider("n_est", min_value=1, max_value=5_000, step=1)
+if st.sidebar.radio('Select model demo',('Tree','K-means')) == 'Tree':
+    st.title('DecisionTreeRegressor vs Adaboost')
+    
+    n_est = st.sidebar.slider("n_est", min_value=1, max_value=5_000, step=1, value=1_000)
 
-y1, y2 = make_predictions(n_est=n_est)
+    y1, y2 = make_predictions(n_est=n_est)
 
-if st.sidebar.checkbox("Toggle ScatterChart"):
-    plt.scatter(x, y, alpha=0.1)
-plt.plot(x, y1, label="just a tree")
-plt.plot(x, y2, label=f"adaboost-{n_est}")
-plt.legend()
-
-st.set_option('deprecation.showPyplotGlobalUse', False)
-st.pyplot()
+    if st.sidebar.checkbox("Toggle ScatterChart"):
+        plt.scatter(x, y, alpha=0.1)
+    plt.plot(x, y1, label="Simple decision tree")
+    plt.plot(x, y2, label=f"adaboost-{n_est}")
+    plt.legend()
+    st.pyplot()
+    
+else:
+    st.title('K-means demo with make_blobs (2D)')
+    st.sidebar.markdown('# Generate random points')
+    nb_points = st.sidebar.slider('Number of points',min_value=0,max_value=1000,value=100, step = 10)
+    std = st.sidebar.slider('Standard deviation',min_value=0.05, max_value=3.0, value=1.0, step=0.5)
+    nb_clusters = st.sidebar.slider('Number of clusters',min_value=1, max_value=10, value=3, step=1, help='How many zones are the points divided into?')
+    if st.sidebar.button('Generate points and run K-means'):
+        points = make_blobs_app(nb_points,nb_clusters,std)[0]
+        X = [item[0] for item in points]
+        Y = [item[1] for item in points]
+        plt.scatter(X,Y)
+        plt.title('Your points')
+        st.pyplot()
+        km = KMeans(n_clusters=nb_clusters)
+        prediction = km.fit_predict(points)
+        plt.scatter(X,Y,c=prediction)
+        plt.title('K-means labeled datapoints like so')
+        st.pyplot()
--- utils.py
+++ utils.py
@@ -1,5 +1,6 @@
 from sklearn.tree import DecisionTreeRegressor
 from sklearn.ensemble import AdaBoostRegressor
+from sklearn.datasets import make_blobs
 import streamlit as st
 import numpy as np
 
@@ -10,8 +11,14 @@ X = np.linspace(0, 6, n)[:, np.newaxis]
 y = np.sin(X).ravel() + np.sin(6 * X).ravel() + np.random.random(n) * 0.3
 
 @st.cache
-def make_predictions(n_est):
+def make_predictions(n_est:int):
+    'uses the trees to make predictions based on the estimator provided'
     mod1 = DecisionTreeRegressor(max_depth=4)
     y1 = mod1.fit(X,y).predict(X)
     y2 = AdaBoostRegressor(mod1, n_estimators=n_est).fit(X, y).predict(X)
     return y1, y2
+
+@st.cache
+def make_blobs_app(n_samples:int, centers:int, cluster_std:float):
+    "generates random points defined by the params chosen by the user, using sklearn's make_blobs function"
+    return make_blobs(n_samples=n_samples, n_features=2, centers=centers, cluster_std=cluster_std, return_centers=False)
